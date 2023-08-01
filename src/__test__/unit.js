47901bdd2736902e92ef543584bdd1716d54a283
sanDream <fs517010856@gmail.com>
2023-08-01 23:50:53 +0800

    add Kth Largest Element and unit test

--- src/__test__/unit.js
+++ src/__test__/unit.js
@@ -1,16 +1,46 @@
-const twoSum = require('../twoSum');
-const findLongestWord = require('../findLongestWord');
+const twoSum = require('../twoSum')
+const findLongestWord = require('../findLongestWord')
+const findKthLargest = require('../findKthLargest')
 
 describe('two sum', () => {
   it('输入：numbers = [2,7,11,15], target = 9', () => {
-    const result = twoSum([2, 7, 11, 15], 9);
-    expect(result).toEqual([1, 2]);
-  });
-});
+    const result = twoSum([2, 7, 11, 15], 9)
+    expect(result).toEqual([1, 2])
+  })
+})
 
 describe('findLongestWord test', () => {
   it('输入：s = "abpcplea", dictionary = ["ale","apple","monkey","plea"]', () => {
-    const result = findLongestWord('abpcplea', ['ale', 'apple', 'monkey', 'plea']);
-    expect(result).toBe('apple');
-  });
-});
+    const result = findLongestWord('abpcplea', [
+      'ale',
+      'apple',
+      'monkey',
+      'plea',
+    ])
+    expect(result).toBe('apple')
+  })
+})
+
+describe('数组中第 K 大的数', () => {
+  it('输入: [3,2,1,5,6,4], k = 2', () => {
+    const result = findKthLargest([3, 2, 1, 5, 6, 4], 2)
+    expect(result).toBe(5)
+  })
+  it('输入: [3,2,3,1,2,4,5,5,6], k = 4', () => {
+    const result = findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)
+    expect(result).toBe(4)
+  })
+  it('输入: [1], k = 1', () => {
+    const result = findKthLargest([1], 1)
+    expect(result).toBe(1)
+  })
+  it('输入: 随机100长度数组', () => {
+    const getRandomInt = (max) => {
+      return Math.floor(Math.random() * max)
+    }
+    const nums = [...Array(100)].map((_) => getRandomInt(100))
+    const k = getRandomInt(99) + 1
+    const result = findKthLargest(nums, k)
+    expect(result).toBe(nums.sort((a, b) => b - a)[k - 1])
+  })
+})
---
+++ src/findKthLargest.js
@@ -0,0 +1,64 @@
+const hasLeftChild = (index, size) => {
+  return 2 * index + 1 < size
+}
+const siftUp = (heap, startIndex) => {
+  let index = startIndex
+  while (index > 0) {
+    const parentIndex = Math.floor((index - 1) / 2)
+    if (heap[index] < heap[parentIndex]) {
+      ;[heap[index], heap[parentIndex]] = [heap[parentIndex], heap[index]]
+    } else {
+      break
+    }
+    index = parentIndex
+  }
+}
+const siftDown = (heap, startIndex) => {
+  let index = startIndex
+  const length = heap.length
+  while (hasLeftChild(index, length)) {
+    const leftChildIndex = 2 * index + 1
+    let smallerChildIndex = leftChildIndex
+    const rightChildIndex = 2 * index + 2
+    if (
+      rightChildIndex < length &&
+      heap[leftChildIndex] > heap[rightChildIndex]
+    ) {
+      smallerChildIndex = rightChildIndex
+    }
+    if (heap[index] <= heap[smallerChildIndex]) {
+      break
+    } else {
+      ;[heap[index], heap[smallerChildIndex]] = [
+        heap[smallerChildIndex],
+        heap[index],
+      ]
+    }
+    index = smallerChildIndex
+  }
+}
+
+/**
+ * @description: 返回数组中第 k 个最大的元素
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number | null}
+ */
+const findKthLargest = function (nums, k) {
+  const heap = []
+  for (let i = 0; i < nums.length; i++) {
+    // 构建最小堆
+    if (i < k) {
+      heap.push(nums[i])
+      siftUp(heap, heap.length - 1)
+    } else {
+      if (nums[i] >= heap[0]) {
+        heap[0] = nums[i]
+        siftDown(heap, 0)
+      }
+    }
+  }
+  return heap[0] || null
+}
+
+module.exports = findKthLargest
